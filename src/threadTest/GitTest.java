91fd1da18e3f54b38a686e9dba55f8022cb5fc44
zhangyn <564011275@qq.com>
2016-07-05 14:44:59 +0800

    my first test

---
+++ src/threadTest/GitTest.java
@@ -0,0 +1,10 @@
+package threadTest;
+
+/**
+ * Created by zhangyn on 2016/7/5.
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("testGitHub");
+    }
+}
